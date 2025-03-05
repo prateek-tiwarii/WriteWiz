d483cc7a36a8ca13273f8fa34e79f104bd63d5fa
prateek tiwari <tiwariprateek1976@gmail.com>
2025-03-05 15:43:42 +0530

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import Land from "./screen/home";
-import { SignupFormDemo } from "./screen/register";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'WriteWiz ⚡',
+  description:
+    'Effortless Blogging , Inspired Writing',
+};
 
 export default function Home() {
   return (
      <>
      <Land/>
-     {/* <SignupFormDemo/> */}
      
      </>
   );
